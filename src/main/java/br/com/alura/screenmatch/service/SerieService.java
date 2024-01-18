81a952dcffbe2bb4d747703f25b4941ab69ff78f
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-01-18 15:15:55 -0300

    Criação de endpoint para consulta de top 5 séries.

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 
 @Service
@@ -16,16 +17,23 @@ public class SerieService {
 	private SerieRepository repositorio;
 	
 	public List<SerieDTO> obterTodasAsSeries() {
-		return repositorio.findAll()
-			.stream()
-			.map(s -> new SerieDTO(s.getId(),
-					s.getTitulo(),
-					s.getTotalTemporadas(),
-					s.getAvaliacao(),
-					s.getGenero(),
-					s.getAtores(),
-					s.getPoster(),
-					s.getSinopse()))
-			.collect(Collectors.toList());
+		return converteDados(repositorio.findAll());
+	}
+
+	public List<SerieDTO> obterTop5Series() {
+		return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+	}
+	
+	private List<SerieDTO> converteDados(List<Serie> series) {
+		return series.stream()
+				.map(s -> new SerieDTO(s.getId(),
+						s.getTitulo(),
+						s.getTotalTemporadas(),
+						s.getAvaliacao(),
+						s.getGenero(),
+						s.getAtores(),
+						s.getPoster(),
+						s.getSinopse()))
+				.collect(Collectors.toList());
 	}
 }
