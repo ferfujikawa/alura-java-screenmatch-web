bbab560f1bd037151be51118298f07a3322e9ea0
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-01-18 14:36:20 -0300

    Conversão do retorno de série utilizando DTO.

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,12 +1,13 @@
 package br.com.alura.screenmatch.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.repository.SerieRepository;
 
 @RestController
@@ -16,7 +17,17 @@ public class SerieController {
 	private SerieRepository repositorio;
 
 	@GetMapping("/series")
-	public List<Serie> obterSeries() {
-		return repositorio.findAll();
+	public List<SerieDTO> obterSeries() {
+		return repositorio.findAll()
+				.stream()
+				.map(s -> new SerieDTO(s.getId(),
+						s.getTitulo(),
+						s.getTotalTemporadas(),
+						s.getAvaliacao(),
+						s.getGenero(),
+						s.getAtores(),
+						s.getPoster(),
+						s.getSinopse()))
+				.collect(Collectors.toList());
 	}
 }
