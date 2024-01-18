24c18dda01da8006a6b489b524223db7558b66eb
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-01-18 16:07:29 -0300

    Implementação de endpoint para obtenção de séries por categoria.

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -50,5 +50,10 @@ public class SerieController {
 	public List<EpisodioDTO> obterTemporadasPorNumero(@PathVariable Long id,
 			@PathVariable Long numero) {
 		return servico.obterTemporadasPorNumero(id, numero);
-	} 
+	}
+	
+	@GetMapping("/categoria/{nomeGenero}")
+	public List<SerieDTO> obterSeriesPorCategoria(@PathVariable String nomeGenero) {
+		return servico.obterSeriesPorCategoria(nomeGenero);
+	}
 }
--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.alura.screenmatch.dto.EpisodioDTO;
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Categoria;
 import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 
@@ -77,5 +78,11 @@ public class SerieService {
 				.stream()
 				.map(e -> new EpisodioDTO(e.getTemporada(), e.getTitulo(), e.getNumeroEpisodio()))
 				.collect(Collectors.toList());
+	}
+
+	public List<SerieDTO> obterSeriesPorCategoria(String nomeGenero) {
+		Categoria categoria = Categoria.fromPortugues(nomeGenero);
+		
+		return converteDados(repositorio.findByGenero(categoria));
 	}
 }
